0f3e095110d743649740bc7c2061ef73a5c9d13d
Fede Mosquera <freddymdq@gmail.com>
2023-02-03 17:58:40 -0300

    Agregado NavBar collapse

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,3 +1,6 @@
+import { Button } from "../Common/Button"
+
+
 export const Hero = () => {
     return (
         <>
@@ -7,7 +10,10 @@ export const Hero = () => {
                         <h1 className="text-5xl font-bold text-black">Bienvenido</h1>
                         <p className="py-6 text-white"><span className="font-bold text-black  font.">DrinkHome</span> puede solicitar información adicinal para verificar la identidad de tu persona o empresa.  
                         <span className="font-bold"> Acepto los terminos y condiciones</span>, junto con las politicas privadas de <span className="font-bold text-black font.">DrinkHome</span> </p>
-                        <button className="btn btn-primary">Soy mayor de edad +18</button>
+                        <Button className="btn btn-primary" type="submit"> 
+                            Soy mayor de edad +18 
+                        </Button>
+                        
                     </div>
                 </div>
             </div>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,29 +1,36 @@
 import CartWidget from "./CartWidget"
 
-
 const NavBar = () => {
     return (
         <>
-            <nav>
-                <div className="navbar bg-neutral">
-                    <div className="flex-1">
-                        <a className="btn btn-ghost normal-case text-xl text-white font-bold">Drink Home</a>
-                    </div>
-                    <div className="flex-none">
-                        <ul className="menu menu-horizontal px-1 text-white">
-                            <li>
-                                <a>Whisky</a>
-                                <a>Spirits</a>
-                                <a>Aperitive</a>                           
-                            </li>
+            <div className="navbar bg-neutral">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-neutral text-white font-bold  rounded-box w-52">
+                            <li><a>Whisky</a></li>
+                            <li><a>Spirits</a></li>
+                            <li><a>Aperitivos</a></li>
                         </ul>
-                        <CartWidget />
                     </div>
-               
+                    <a className="btn btn-ghost normal-case text-xl text-white">DRINHOME</a>
                 </div>
-            </nav>
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 text-white font-bold">
+                        <li><a>Whisky</a></li>
+                        <li><a>Spirits</a></li>     
+                        <li><a>Aperitivos</a></li>
+                    </ul>
+                </div>
+                <CartWidget/>
+            </div>
         </>
     )
 }
 
+
 export default NavBar
+
+
