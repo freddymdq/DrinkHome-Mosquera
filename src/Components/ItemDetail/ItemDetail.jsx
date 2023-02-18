dc95f471a475805d01ea068ba627b63b752796c5
Fede Mosquera <freddymdq@gmail.com>
2023-02-18 11:05:05 -0300

    Error orden de compra no se guarda en firestore

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -26,7 +26,7 @@ export const ItemDetail = ({ data }) => {
       <div className="mx-auto text-center bg-[#151515] flex flex-wrap  font-bold  justify-beetwend text-white pt-[30px] p-[50px]">
           <div key={data.id} className="container-lg  mt-5 w-[450px]  rounded-[50px]  bg-black mb-[30px] mx-auto relative flex justify-center ">
             <section className="container  bg-black items-center justify-center rounded-[50px] px-8 py-8 flex flex-col">
-              <p className="text-warning font-black text-5xl pb-2 mt-5">{data.title}</p>
+              <p className="text-warning font-black text-3xl pb-2 mt-5">{data.title}</p>
               <img src= {data.foto} width="180px" alt="Whisky"/>
               <h2 className="text-white font-bold text-3xl text-center">{data.description}</h2>
               <p className="text-base-300 pt-1 text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus voluptatibus, explicabo fugiat nam atque quo maxim.</p>
